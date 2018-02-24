24451585e61f1066b0e65d31b4b2acc4e81f5110
DisseDev <30370034+DisseDev@users.noreply.github.com>
2018-02-24 19:08:47 +0000

    Version Upped to DWT 1.1
    Version Upped to DWT 1.1

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Disse");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "CWT"
+#define CLIENT_VERSION_SUFFIX   "-DWT"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
